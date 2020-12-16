34a0f6097aaaf217d9eadc5d69dff52ed7e1a0c3
Márcio Duarte <zmduartez@gmail.com>
2020-12-16 10:34:14 +0000

    Add ip forwarding

--- demo/tux4.sh
+++ demo/tux4.sh
@@ -10,3 +10,7 @@ ifconfig eth1 172.16.31.253/24
 
 # sets the rc router as the default route
 route add default gw 172.16.31.254
+
+# configure router
+echo 1 > /proc/sys/net/ipv4/ip_forward
+echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
